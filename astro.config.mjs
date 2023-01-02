f0e0fce0d91fca8e52fc73c4776f18e6e713122f
Shu Gen <gen_shu@caddi.jp>
2023-01-02 19:34:28 +0900

    introduce prettier to format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
-import astroI18next from "astro-i18next";
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://genkey6.dev',
-	integrations: [astroI18next(), mdx(), sitemap()],
+  site: 'https://genkey6.dev',
+  integrations: [astroI18next(), mdx(), sitemap()],
 });
--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'genkey6.dev';
-export const SITE_DESCRIPTION = 'genkey6\'s Blog.';
+export const SITE_DESCRIPTION = "genkey6's Blog.";
