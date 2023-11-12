00351dbffe23ea1ef23b7221aecf8211add1bd35
Gen Shu <yuan.zhu@10x.co.jp>
2023-11-12 12:18:42 +0900

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import vercelEdge from '@astrojs/vercel/edge';
 
 import sitemap from '@astrojs/sitemap';
 import astroI18next from 'astro-i18next';
@@ -7,5 +8,7 @@ import astroI18next from 'astro-i18next';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://genkey6.dev',
+  output: 'server',
   integrations: [astroI18next(), mdx(), sitemap()],
+  adapter: vercelEdge(),
 });
